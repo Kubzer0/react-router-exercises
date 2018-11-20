71dbdfcee228ebfc7761f02b1c7cda0a168c840b
kubzer0 <jakub.gryka@gmail.com>
2018-11-20 14:50:14 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Route1 from './Route1'
 import Route2 from './Route2'
@@ -9,14 +9,21 @@ import Route3 from './Route3'
 const App = (props) =>
   (
     <div>
-      <BrowserRouter>
+      <Router>
         <div>
-          <Route path="/route-1" exact={true} component= {Route1}></Route>
-          <Route path="/route-2" exact={true} component= {Route2}></Route>
-          <Route path="/route-3" exact={true} component= {Route3}></Route>
-          
+          <div>
+            <ul>
+              <li><Link to='/route-1'>Route1</Link></li>
+              <li><Link to='/route-2'>Route2</Link></li>
+              <li><Link to='/route-3'>Route3</Link></li>
+            </ul>
+          </div>
+          <Route path="/route-1" exact={true} component={Route1}></Route>
+          <Route path="/route-2" exact={true} component={Route2}></Route>
+          <Route path="/route-3" exact={true} component={Route3}></Route>
+
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   )
 
